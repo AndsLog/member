85968557afc551172f7842a80a33a6e03aafb773
AndsLog <leanjn0922@gmail.com>
2018-09-26 10:06:04 +0800

    feature of insert and update ok

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,81 @@
-$(document).ready(function() {
-    getMembers();
-});
+(function () {
+    var memberId;
 
-function getMembers() {
-    let apiUrl = 'http://localhost:3000/api/take';
-    let reqInit = {
-        "headers": new Headers({'Content-Type': 'application/json'}),
-        "method": 'GET'
+    $(document).ready(function() {
+        getMembers();
+    });
+    
+    function getMembers() {
+        let apiUrl = window.location.origin + '/api/take';
+        let reqInit = {
+            "headers": new Headers({'Content-Type': 'application/json'}),
+            "method": 'GET'
+        }
+    
+        return window.fetch(apiUrl, reqInit).then(function(res) {
+            res.json().then(function(resJson) {
+                let docs = resJson['docs'];
+                // let docs = resJson.docs;
+                // for(let i = 0; i< docs.length; i ++){
+                //     let name = docs[i].name;
+                //     let age = docs[i].age;
+                //     let sex = docs[i].sex;
+                //     let id = docs[i]._id;
+                //     let trElement = `<tr data-memberId="${id}">
+                //                         <td>${name}</td>
+                //                         <td>${age}</td>
+                //                         <td>${sex}</td>
+                //                     </tr>`;
+                //     $('#member-list').append(trElement);
+                // }
+                // let trElement;
+                docs.map(function(doc) {
+                    let name = doc.name;
+                    let age = doc.age;
+                    let sex = doc.sex;
+                    let id = doc._id;
+                    let trElement = `<tr id="${id}">
+                                        <td id="td-name">${name}</td>
+                                        <td id="td-age">${age}</td>
+                                        <td id="td-sex">${sex}</td>
+                                        <td>
+                                            <button type="button" id="delete">刪除</button>
+                                            <button type="button" id="update" data-toggle="modal" data-target="#update-modal">修改</button>
+                                        </td>
+                                    </tr>`;
+                    $('#member-list').append(trElement);
+                });
+                // $('#member-list').append(trElement);
+            })
+        })
     }
-
-    return window.fetch(apiUrl, reqInit).then(function(res) {
-        res.json().then(function(resJson) {
-            let docs = resJson['docs'];
-            // // let docs = resJson.docs;
-            // for(let i = 0; i< docs.length; i ++){
-            //     let name = docs[i].name;
-            //     let age = docs[i].age;
-            //     let sex = docs[i].sex;
-            //     let id = docs[i]._id;
-            //     let trElement = `<tr data-memberId="${id}">
-            //                         <td>${name}</td>
-            //                         <td>${age}</td>
-            //                         <td>${sex}</td>
-            //                     </tr>`;
-            //     $('#member-list').append(trElement);
-            // }
-            // let trElement;
-            docs.map(function(doc) {
-                let name = doc.name;
-                let age = doc.age;
-                let sex = doc.sex;
-                let id = doc._id;
-                let trElement = `<tr data-memberId="${id}">
+    
+    $(document).on('click', '#submit', function() {
+        let name = $('#name').val();
+        let age = $('#age').val();
+        let sex = $('#sex').val();
+    
+        let apiUrl = window.location.origin + '/api/create';
+        let reqInit = {
+            "headers": new Headers({'Content-Type': 'application/json'}),
+            "method": 'POST',
+            "body": JSON.stringify({
+                "name_key": name,
+                "age_key": age,
+                "sex_key": sex
+            })
+        }
+    
+        console.log('name:' + name + ', age:' + age + ', sex:' + sex);
+        console.log(`name:${name}, age: ${age}, sex: ${sex}`); //模板字串
+    
+        return window.fetch(apiUrl, reqInit).then(function (res) {
+            res.json().then(function (resJson) {
+                let id = resJson.id_key;
+                let name = resJson.name;
+                let age = resJson.age;
+                let sex = resJson.sex;
+                let trElement = `<tr id="${id}">
                                     <td id="td-name">${name}</td>
                                     <td id="td-age">${age}</td>
                                     <td id="td-sex">${sex}</td>
@@ -40,63 +84,57 @@ function getMembers() {
                                         <button type="button" id="update" data-toggle="modal" data-target="#update-modal">修改</button>
                                     </td>
                                 </tr>`;
+                console.log(resJson);
                 $('#member-list').append(trElement);
-            });
-            // $('#member-list').append(trElement);
+            })
+            console.log(res);
         })
-    })
-}
+    });
+    
+    $(document).on('click', '#update', function() {
+        let tdName = $(this).parent().parent().find('#td-name').text();
+        let tdAge = $(this).parent().parent().find('#td-age').text();
+        let tdSex = $(this).parent().parent().find('#td-sex').text();
+    
+        // 重要!! 取得id的方法，.attr('data-memberId')指的是取得屬性data-memberId的值
+        memberId = $(this).parent().parent().attr('id');
+    
+        $('#name_m').val(tdName);
+        $('#age_m').val(tdAge);
+        $('#sex_m').val(tdSex);
+    });
+    
+    $(document).on('click', '#update-btn', function() {
+        let modalName = $('#name_m').val();
+        let modalAge = $('#age_m').val();
+        let modalSex = $('#sex_m').val();
 
-$(document).on('click', '#submit', function() {
-    let name = $('#name').val();
-    let age = $('#age').val();
-    let sex = $('#sex').val();
+        let apiUrl = window.location.origin + '/api/update';
+        let reqInit = {
+            "headers": new Headers({'Content-Type': 'application/json'}),
+            "method": 'PUT',
+            "body": JSON.stringify({
+                "name_key": modalName,
+                "age_key": modalAge,
+                "sex_key": modalSex,
+                "id_key": memberId
+            })
+        }
 
-    let apiUrl = 'http://localhost:3000/api/create';
-    let reqInit = {
-        "headers": new Headers({'Content-Type': 'application/json'}),
-        "method": 'POST',
-        "body": JSON.stringify({
-            "name_key": name,
-            "age_key": age,
-            "sex_key": sex
-        })
-    }
+        return window.fetch(apiUrl, reqInit).then(function (res) {
+            res.json().then(function(resJson) {
+                let name = resJson.name;
+                let age = resJson.age;
+                let sex = resJson.sex;
+                let id = resJson._id;
 
-    console.log('name:' + name + ', age:' + age + ', sex:' + sex);
-    console.log(`name:${name}, age: ${age}, sex: ${sex}`); //模板字串
-
-    return window.fetch(apiUrl, reqInit).then(function (res) {
-        res.json().then(function (resJson) {
-            let id = resJson.id_key;
-            let name = resJson.name;
-            let age = resJson.age;
-            let sex = resJson.sex;
-            let trElement = `<tr data-memberId="${id}">
-                                <td id="td-name">${name}</td>
-                                <td id="td-age">${age}</td>
-                                <td id="td-sex">${sex}</td>
-                                <td>
-                                    <button type="button" id="delete">刪除</button>
-                                    <button type="button" id="update" data-toggle="modal" data-target="#update-modal">修改</button>
-                                </td>
-                            </tr>`;
-            console.log(resJson);
-            $('#member-list').append(trElement);
-        })
-        console.log(res);
-    })
-});
-
-$(document).on('click', '#update', function() {
-    let tdName = $(this).parent().parent().find('#td-name').text();
-    let tdAge = $(this).parent().parent().find('#td-age').text();
-    let tdSex = $(this).parent().parent().find('#td-sex').text();
-
-    // 重要!! 取得id的方法，.attr('data-memberId')指的是取得屬性data-memberId的值
-    let memberId = $(this).parent().parent().attr('data-memberId');
-
-    $('#name_m').val(tdName);
-    $('#age_m').val(tdAge);
-    $('#sex_m').val(tdSex);
-})
+                $('#' + id).find('#td-name').text(name);
+                $('#' + id).find('#td-age').text(age);
+                $('#' + id).find('#td-sex').text(sex);
+            }).then(function() {
+                $('#update-modal').modal('hide');
+            })
+        });
+    });
+    
+}());
--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,8 @@ router.post('/create', function(req, res) {
         "name": name,
         "age": age,
         "sex": sex,
-        "timestamp": Date.now()
+        "timestamp": Date.now(),
+        "id_key": ''
     }
     Cloudant({account:username, password:password}, function(err, cloudant) {
         if (err) {
@@ -20,13 +21,14 @@ router.post('/create', function(req, res) {
         }
 
         var db = cloudant.db.use('member');
-        db.insert(pushData, function(err, data) {
+        db.insert(pushData, function(err, result) {
             if (err) {
                 return console.log('Failed to insert member: ' + err.message);
             }
 
-            let id = data.id;
+            let id = result.id;
             pushData.id_key = id;
+            pushData['id_key'] = id;
             res.status(200).json(pushData);
         });
     });
@@ -49,12 +51,51 @@ router.get('/take', function(req, res) {
         }
         db.find(query, function(err, data) {
             if (err) {
-                return console.log('Failed to insert member: ' + err.message);
+                return console.log('Failed to find member: ' + err.message);
             }
             console.log(data);
             res.status(200).json(data);
         });
     });
+});
+
+router.put('/update', function(req, res) {
+    let name = req.body.name_key;
+    let age = req.body.age_key;
+    let sex = req.body.sex_key;
+    let id = req.body.id_key;
+    Cloudant({account:username, password:password}, function(err, cloudant) {
+        if (err) {
+          return console.log('Failed to initialize Cloudant: ' + err.message);
+        }
+        var db = cloudant.db.use('member');
+        let query = {
+            "selector": {
+                "_id": {
+                   "$eq": id
+                }
+             },
+             "sort": [{"timestamp": "asc"}]
+        }
+        db.find(query, function(err, data) {
+            if (err) {
+                return console.log('Failed to find member: ' + err.message);
+            }
+            console.log(data);
+            let doc = data.docs[0];
+            doc.name = name;
+            doc.age = age;
+            doc.sex = sex;
+            doc.updateTime = Date.now();
+            db.insert(doc, function(err, result) {
+                if (err) {
+                    return console.log('Failed to update member: ' + err.message);
+                }
+                res.status(200).json(doc);
+            })
+        });
+    })
+
 })
 
 module.exports = router;
